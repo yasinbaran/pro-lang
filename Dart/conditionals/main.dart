cc15505230f2c467ae6c9e06a765533778b13a9f
yasinbaran <yasin.baran@bil.omu.edu.tr>
2019-03-27 01:11:49 +0300

    formatting

--- Dart/conditionals/main.dart
+++ Dart/conditionals/main.dart
@@ -2,25 +2,27 @@
 
 void main() {
   var sayi = 3;
-  if(sayi > 3){
-      print("Sayi 3'ten büyüktür.");
-  } else if(sayi==3){
-      print("Sayi 3'tür.");
+  if (sayi > 3) {
+    print("Sayi 3'ten büyüktür.");
+  } else if (sayi == 3) {
+    print("Sayi 3'tür.");
   } else {
-      print("Sayi 3'ten küçüktür.");
+    print("Sayi 3'ten küçüktür.");
   }
 
   var plakaKodu = 66;
 
   switch (plakaKodu) {
-    case 01: {print("Adana");}
-    break;
-    case 55: {print("Samsun");}  
-    break;
-    case 66: {print("YOZGAT");}
-    break;
-    default: {print("Bilinmiyor.");}
+    case 01:
+      print("Adana");
+      break;
+    case 55:
+      print("Samsun");
+      break;
+    case 66:
+      print("YOZGAT");
+      break;
+    default:
+      print("Bilinmiyor.");
   }
-
-
 }
