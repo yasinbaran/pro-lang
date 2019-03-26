8be403674642ea8efec92e44fd39112043c8aa7c
yasinbaran <yasin.baran@bil.omu.edu.tr>
2019-03-27 01:03:10 +0300

    variables and conditionals

---
+++ Dart/conditionals/main.dart
@@ -0,0 +1,26 @@
+//Yasin BARAN
+
+void main() {
+  var sayi = 3;
+  if(sayi > 3){
+      print("Sayi 3'ten büyüktür.");
+  } else if(sayi==3){
+      print("Sayi 3'tür.");
+  } else {
+      print("Sayi 3'ten küçüktür.");
+  }
+
+  var plakaKodu = 66;
+
+  switch (plakaKodu) {
+    case 01: {print("Adana");}
+    break;
+    case 55: {print("Samsun");}  
+    break;
+    case 66: {print("YOZGAT");}
+    break;
+    default: {print("Bilinmiyor.");}
+  }
+
+
+}
---
+++ Dart/variables/main.dart
@@ -0,0 +1,14 @@
+//Yasin BARAN
+
+void main() {
+  
+  String name = "Yasin";
+  String surName = "BARAN";
+  int yas = 25;
+  double boy = 1.75;
+  bool erkekMi =true;
+
+  print(name +" "+ surName +" "+ yas.toString() +" yaşındadır ve " + boy.toString() + " m boyundadır." );
+  print(erkekMi);
+
+}
