3eb2d116463399165011669fdc2642fad20aac19
Yasin  BARAN <yasin.baran@bil.omu.edu.tr>
2019-03-28 19:41:51 +0300

    Update main.dart

--- Dart/variables/main.dart
+++ Dart/variables/main.dart
@@ -6,9 +6,12 @@ void main() {
   String surName = "BARAN";
   int yas = 25;
   double boy = 1.75;
+  var memleket = 'YOZGAT';
   bool erkekMi =true;
 
   print(name +" "+ surName +" "+ yas.toString() +" yaşındadır ve " + boy.toString() + " m boyundadır." );
   print(erkekMi);
+  
+  print('$name $surname $yas yaşındadır ve $memleket lıdır.');
 
 }
